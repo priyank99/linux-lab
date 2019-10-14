8e511cee9c4a62a96acc8169833d2234edfaf051
priyank99 <priyank9x@gmail.com>
2019-10-15 00:05:24 +0530

    added p12

--- p11.sh
+++ p11.sh
@@ -1,1 +1,7 @@
-find ~ -maxdepth 1 -user $(whoami) -perm -u+rwx | sort
+if [ $# != 0 ]
+then
+    location=$1
+    find $location -maxdepth 1 -user $(whoami) -perm -u+rwx | sort
+else
+    echo "no location specified"
+fi
--- p12.sh
+++ p12.sh
@@ -1,1 +1,20 @@
-find . -maxdepth 1 -user $(whoami) -perm -u+rwx | sort
+
+function remove {
+    echo removing $2
+    filename=$1
+    word=$2
+    filename2=${filename}
+    filename2+=".out"
+
+    echo $filename2
+    grep -v $word $filename > $filename2
+    # ; mv $filename2 $filename
+}
+
+echo $#
+args=("$@") 
+word=$1
+for (( i = 1; i < $# ; i++ ))
+do
+    remove ${args[${i}]} $word
+done
